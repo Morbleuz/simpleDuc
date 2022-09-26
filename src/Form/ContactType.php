3356c7e240f57e5c635f66887c35c18f357b3904
Axel <axel2003@outlook.com>
2022-09-26 16:37:47 +0200

    Ajout de la section contact, fonctionnelle, ajout du padding dans le body pour evité le problème avec la navbar

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,15 +5,20 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('sujet')
-            ->add('commentaire')
+            ->add('email', EmailType::class,['label'=> false, 'attr' => ['class'=> 'w3-input w3-border', 'placeholder' => 'Email']])
+            ->add('sujet', TextType::class,['label'=> false, 'attr' => ['class'=> 'w3-section w3-input w3-border', 'placeholder' => 'Sujet']])
+            ->add('commentaire', TextAreaType::class,['label'=> false, 'attr' => ['class'=> 'w3-section w3-input w3-border', 'placeholder' => 'Commentaire']])
+            ->add('envoyer', SubmitType::class,['label'=> 'Envoyer', 'attr' => ['class'=> 'w3-button w3-black w3-section']] )
         ;
     }
 
